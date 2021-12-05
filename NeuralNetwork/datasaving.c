1ecc9de06fb35e99c95763ee30cce7a227f33e13
Beunel <maxence.beunel@epita.fr>
2021-12-05 15:32:23 -0500

    data

--- NeuralNetwork/datasaving.c
+++ NeuralNetwork/datasaving.c
@@ -5,9 +5,10 @@
 
 int writeList(double* data, size_t size, char* name)
 {
-    FILE *fp;
+    FILE* fp;
     fp = fopen(name, "w");
-    if (fp == NULL) return 1;
+    if (fp == NULL)
+    	return 1;
     fwrite(data, sizeof(double) * size, 1, fp);
     fclose(fp);
     return 0;
@@ -60,26 +61,31 @@ NeuralNetwork* readNetwork()
     if (network == NULL)
         errx(1, "Memory allocation failed");
     fread(network, sizeof(size_t) * 4, 1, netFp);
+    
+    //network->activations = malloc(sizeof(double) * (network->inputNumber + network->hiddenNumber + network->outputNumber));
     network->activations = readList("save/activations.save", network->inputNumber + network->hiddenNumber + network->outputNumber);
-    network->hiddenWeights = readList("save/hWeights.save", "save/hWeights_l.save");
-    network->inputWeights = readList("save/iWeights.save", "save/iWeights_l.save");
-    network->bias = readList("save/bias.save", "save/bias_l.save");
-    network->asciiOutputs = readList("save/outAscii.save", "save/outAscii_l.save");
-
-    if (network->bias == NULL)
-        printf("Unable to load bias\n");
-
-    if (network->hidenWeights == NULL)
-        printf("Unable to load hiden weights\n");
-
-    if (network->inputWeights == NULL)
-        printf("Unable to load input wieghts\n");
-
     if (network->activations == NULL)
-        printf("Unable to load activations\n");
-
+        errx(1, "Unable to load activations\n");
+    
+    //network->bias = malloc(sizeof(double) * (network->hiddenNumber + network->outputNumber));
+    network->bias = readList("save/bias.save", network->hiddenNumber + network->outputNumber);
+    if (network->bias == NULL)
+        errx(1, "Unable to load bias\n");
+    
+    //network->inputWeights = malloc(sizeof(double) * (network->inputNumber * network->hiddenNumber));
+    network->inputWeights = readList("save/inputWeights.save", network->inputNumber * network->hiddenNumber);
+    if (network->inputWeights == NULL)
+        errx(1, "Unable to load inputWeights\n");
+    
+    //network->hiddenWeights = malloc(sizeof(double) * (network->hiddenNumber * network->outputNumber));
+    network->hiddenWeights = readList("save/hiddenWeights.save", network->hiddenNumber * network->outputNumber);
+    if (network->hiddenWeights == NULL)
+        errx(1, "Unable to load hiddenWeights\n");
+    
+    //network->asciiOutputs = malloc(sizeof(double) * network->outputNumber);
+    network->asciiOutputs = readList("save/asciiOutputs.save", network->outputNumber);
     if (network->asciiOutputs == NULL)
-        printf("Unable to load ascii output\n");
-
+        errx(1, "Unable to load asciiOutputs\n");
+    
     return network;
 }
