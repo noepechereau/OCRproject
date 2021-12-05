356e156bed5226702e799b0d129636fdcb5ca12b
Eliott Chiche <eliott.chiche@epita.fr>
2021-12-06 00:44:47 +0100

    train

---
+++ jpg_train.h
@@ -0,0 +1,10 @@
+#ifndef JPG_TRAIN_H
+#define JPG_TRAIN_H
+
+#include <SDL2/SDL.h>
+#include <stdbool.h>
+#include "struct/pixel.h"
+
+int* image_for_train(SDL_Surface* image);
+
+#endif //OCR_GRILLE_DETECTION_H_INCLUDED
