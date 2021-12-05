d2930de2fd4abe39f2a8dbe38082dcbe9f0f4b94
noepechereau <noe.pechereau@epita.fr>
2021-12-05 22:25:14 +0100

    add neural

---
+++ NeuralNetwork2/headers/BackProp.h
@@ -0,0 +1,14 @@
+#ifndef BACK_PROP_H
+#define BACK_PROP_H
+#include"NeuralNetworkTools.h"
+
+
+double delta(double error, double d, double v);
+double* outputError(NeuralNetwork* network, double* expected, double* res);
+double hidenError(NeuralNetwork* network, size_t nodeIndex, double* outputError);
+double* hidenErrors(NeuralNetwork* network, double* outputError, double* res);
+double* outputWeightDelta(NeuralNetwork* network, double* errors, double v, double* res);
+double* biasDelta(size_t len, double* errors, double v, double* res);
+double* hidenWeightDelta(NeuralNetwork* network, double* errors, double v, double* res);
+
+#endif
